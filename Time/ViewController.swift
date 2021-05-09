8f766d54d6050ce19f8ff2da0cad13dc70ca5d68
Muhammadjon <20mamarasulov@gmail.com>
2021-05-09 21:21:27 +0900

    Initial Commit

---
+++ Time/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Time
+//
+//  Created by Muhammadjon Mamarasulov on 5/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
